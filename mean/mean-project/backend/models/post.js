f7725ce0e187c6667c3d99edd6fa23427b3da769
Christin <73511883+piaoxianer@users.noreply.github.com>
2022-11-16 11:55:16 -0500

    Convert inputs to form in post-image component. Add a img element in post-list component.

--- mean/mean-project/backend/models/post.js
+++ mean/mean-project/backend/models/post.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 
 // first of all create a blueprint for how your data should look like
 const postSchema = mongoose.Schema({
-  title: { type: String, required: true},
-  content: { type: String, required: true}
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  imagePath: {type: String, required: true }
 });
 
 module.exports = mongoose.model('Post', postSchema);
--- mean/mean-project/src/app/services/posts.service.ts
+++ mean/mean-project/src/app/services/posts.service.ts
@@ -26,6 +26,7 @@ export class PostsService {
               title: post.title,
               content: post.content,
               id: post._id,
+              imagePath: post.imagePath,
             };
           });
         })
@@ -41,30 +42,45 @@ export class PostsService {
   }
 
   getEditPost(id: string) {
-    return this.http.get<{ _id: string; title: string; content: string }>(
-      'http://localhost:3000/api/posts/' + id
-    );
+    return this.http.get<{
+      _id: string;
+      title: string;
+      content: string;
+      imagePath: string;
+    }>('http://localhost:3000/api/posts/' + id);
   }
 
-  addPost(title: string, content: string) {
-    const post: Post = { id: '', title: title, content: content };
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append('title', title);
+    postData.append('content', content);
+    postData.append('image', image, title);
     this.http
-      .post<{ message: string; postId: string }>(
+      .post<{ message: string; post: Post }>(
         'http://localhost:3000/api/posts',
-        post
+        postData
       )
       .subscribe((responseData) => {
         console.log(responseData.message);
-        const id = responseData.postId;
-        post.id = id;
+        const post = {
+          id: responseData.post.id,
+          title: title,
+          content: content,
+          imagePath: responseData.post.imagePath,
+        };
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
         this.router.navigate(['/']);
       });
   }
 
-  updatePost(id: string, title: string, content: string) {
-    const post: Post = { id: id, title: title, content: content };
+  updatePost(id: string, title: string, content: string, image: File) {
+    const post: Post = {
+      id: id,
+      title: title,
+      content: content,
+      imagePath: '',
+    };
     console.log(title);
     this.http
       .put('http://localhost:3000/api/posts/' + id, post)
