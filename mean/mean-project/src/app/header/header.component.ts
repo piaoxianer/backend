e33db9f1c3ab71c12ba5b071c16525e26e841a6e
Christin <73511883+piaoxianer@users.noreply.github.com>
2022-12-16 00:20:45 -0500

    Changed UI of header component
    Changed the logic of displaying the Login, Signup, New post and Logout buttons in the header.

--- mean/mean-project/src/app/header/header.component.ts
+++ mean/mean-project/src/app/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userAuthenticated = false;
+  private authListenerSubs!: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.userAuthenticated = isAuthenticated;
+      });
   }
 
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
 }
