429fc4b2445307fe21114a4ca950a9eedf5185c0
Christin <73511883+piaoxianer@users.noreply.github.com>
2022-12-16 18:03:53 -0500

    Reflected the token expiration in the UI
    It will be automatically logout after 1h or 3600 seconds after login.

--- mean/mean-project/src/app/auth/auth.service.ts
+++ mean/mean-project/src/app/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { AuthData } from './auth-data.model';
 export class AuthService {
   private isAuthenticated = false;
   private token!: string | null;
+  private tokenTimer!: NodeJS.Timer;
   private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) {}
@@ -39,11 +40,19 @@ export class AuthService {
     const authData: AuthData = { email: email, password: password };
     console.log(authData);
     this.http
-      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
+      .post<{ token: string; expiresIn: number }>(
+        'http://localhost:3000/api/user/login',
+        authData
+      )
       .subscribe((response) => {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000);
+
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
         }
@@ -55,6 +64,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(['/']);
   }
 }
