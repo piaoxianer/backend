ca2452c78d900ca349f1231ae979946d5e761b02
Christin <73511883+piaoxianer@users.noreply.github.com>
2022-12-23 14:40:53 -0500

    Modified spinner display logic on post-create page

--- mean/mean-project/src/app/posts/post-create/post-create.component.ts
+++ mean/mean-project/src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 import { Post } from 'src/app/interfaces/post.interface';
 
 import { PostsService } from 'src/app/posts/posts.service';
@@ -11,22 +13,30 @@ import { mimeType } from './mime-type.validator';
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css'],
 })
-export class PostCreateComponent implements OnInit {
+export class PostCreateComponent implements OnInit, OnDestroy {
   enteredTitle = '';
   enteredContent = '';
   form!: FormGroup;
-  private mode = 'create';
-  private postId: any;
   post: Post = {} as Post;
   isLoading = false;
   imagePreview: string = '';
+  private mode = 'create';
+  private postId: any;
+  private authStatusSub?: Subscription;
 
   constructor(
     private postService: PostsService,
-    public route: ActivatedRoute
+    public route: ActivatedRoute,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe((authStatus) => {
+        this.isLoading = false;
+      });
+
     this.form = new FormGroup({
       title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)],
@@ -98,5 +108,9 @@ export class PostCreateComponent implements OnInit {
       );
     }
     this.form.reset();
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub?.unsubscribe();
   }
 }
